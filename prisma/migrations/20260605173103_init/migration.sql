4c1b35c4b7c00b9a5a625b88a17d142e7f913145
BainTheCreator <kottom615@gmail.com>
2026-06-06 00:36:34 +0700

    feat: prisma init

---
+++ prisma/migrations/20260605173103_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "admins" (
+    "id" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_password_key" ON "admins"("password");
